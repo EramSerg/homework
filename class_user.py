c89168b55f6d878bbad12482504d9674c6929291
Eram Sergey <139155326+EramSerg@users.noreply.github.com>
2024-01-12 17:43:48 +0300

    Update class_user.py
    Update class_user.py

--- class_user.py
+++ class_user.py
@@ -1,8 +1,49 @@
 import re
+import itertools
+
+
+class Address:
+    def __init__(self, post_index: int, country: str, city: str, district: str, street: str, home_number: int, flat_number: int):
+        """
+        Создание и подготовка к работе объекта адресс пользователя
+        :param post_index: почтовый индекс
+        :param country: страна
+        :param city: город
+        :param district: район
+        :param street: улица
+        :param home_number: номер дома
+        :param flat_number: номер квартиры
+        """
+        if not isinstance(post_index, int):
+            raise TypeError("Неверный формат почтового индекса, должен быть integer.")
+        self.post_index = post_index
+
+        if not self.validation_country(country):
+            raise ValueError("Укаазана недопустимая страна.")
+        self.country = country
+
+        self.city = city
+        self.district = district
+        self.street = street
+        self.home_number = home_number
+        self.flat_number = flat_number
+
+    def validation_country(self, country):
+        """
+        Функция проверки существования страны (список стран дан просто для примера, в идеале,
+        список стран выделяется в отдельный файл и импортируется
+        :param country: страна пользователя
+        :return: True или False в зависимотси от успешности валидации
+        """
+        countries = ["Россия", "Соединенные Штаты Америки", "Германия", "Финляндия", "Казахстан", "Китай", "Алжир", "Япония"]
+        if country not in countries:
+            return False
+
+        return True
 
 
 class User:
-    def __init__(self, name: str, last_name: str, nicname: str, age: int, user_age_category: str, email: str, phone_number: str, address: object, login: str, password: str):
+    def __init__(self, name: str, last_name: str, nicname: str, age: int, email: str, phone_number: str, address: Address, login: str, password: str):
         """
         Создание и подготовка к работе объекта Пользователь
         :param name: имя пользователя
@@ -30,15 +71,13 @@ class User:
         if not isinstance(age, int):
             raise TypeError("Неверный формат ввода возраста пользователя, должен быть тип integer.")
         self.age = age
-        AgeGroupOfUser.validate_age = user_age_category     # так можно делать или как правильно определять в данном случае?
+        self.age_group = AgeGroupOfUser(self.age).age_group     # так можно делать или как правильно определять в данном случае?
 
-        if not self.validation_email:
-            raise ValueError("Формат email не прошел валидацию, проверьте корректность заполнения данных.")
-        self.email = email
+        self.email = self.validation_email(email)
 
-        if not self.validation_phone_number:
+        if not self.validation_phone_number(phone_number):
             raise ValueError("Формат номера телефона не прошел валидацию, проверьте корректность заполнения данных.")
-        self.format_phone_numbers = phone_number
+        self.phone_number = self.format_phone_numbers(phone_number)
 
         self.nicname = nicname
         self.address = address  # как правильно сослаться на class Address?
@@ -54,8 +93,9 @@ class User:
         pattern = r"^[-\w\.]+@([-\w]+\.)+[-\w]{2,4}$"
 
         if re.match(pattern, email) is None:
-            return False
-        return True
+            raise ValueError("Формат email не прошел валидацию, проверьте корректность заполнения данных.")
+
+        return email
 
     def validation_phone_number(self, phone_number: str):
         """
@@ -80,75 +120,46 @@ class User:
 
 
 class AgeGroupOfUser:
-    def __init__(self, age: int, age_category: str):
+    categories = [0, 6, 12, 18]
+
+    def __init__(self, age: int):
         """
         Создание и подготовка к работе объекта возрастная группа пользователя
         :param age: возраст пользователя
         """
 
-        if not isinstance(age, int):
-            raise TypeError("Неверный формат ввода возраста пользователя, должен быть тип integer.")
-        self.age = age
+        self.age_group = self.validate_age(age)
 
-        self.validate_age = age_category
-
-    def validate_age(self, age: int, category: str):
+    def validate_age(self, age: int):
         """
         Фугкция проверки возраста на соответствие категории
         :param age: возраст пользователя
-        :param category: возрастная категория пользователя (0+, 6+, 12+, 18+)
         :return: строку с указанием возрастной категории пользователя
         """
-        categories = ['0+', '6+', '12+', '18+']
+
         if not isinstance(age, int):
             raise TypeError("Неверный формат ввода возраста пользователя, должен быть тип integer.")
-        if category not in categories:
-            raise ValueError("Выбрана несуществующая возрастная категория.")
-        if 0 < age < 6:
-            return "Возрастная категория 0+"
-        elif 6 <= age < 12:
-            return "Возрастная категория 6+"
-        elif 12 <= age < 18:
-            return "Возрастная категория 12+"
-        elif age >= 18:
-            return "Возрастная категория 18+"
+
+        # Можно проверить что возраст не отрицательный
+
+        # Как аналог коду ниже
+        for min_age, max_age in itertools.pairwise(
+                self.categories):  # К классовым атрибутам можно также обращаться через self (так как они лежат и в области памяти экземпляра)
+            if age < max_age:
+                return f"Возрастная категория {min_age}+"
+        return f"Возрастная категория {max_age}+"
 
 
-class Address:
-    def __init__(self, post_index: int, country: str, city: str, district: str, street: str, home_number: int, flat_number: int):
-        """
-        Создание и подготовка к работе объекта адресс пользователя
-        :param post_index: почтовый индекс
-        :param country: страна
-        :param city: город
-        :param district: район
-        :param street: улица
-        :param home_number: номер дома
-        :param flat_number: номер квартиры
-        """
-        if not isinstance(post_index, int):
-            raise TypeError("Неверный формат почтового индекса, должен быть integer.")
-        self.post_index = post_index
+if __name__ == "__main__":
+    a_g = AgeGroupOfUser(13)
+    print(a_g.age_group)
+    try:
+        print(Address(post_index=1, country=1, city=1, district=1, street=1, home_number=1, flat_number=1))
+    except ValueError as e:
+        print(f"Ошибка: {e}")
 
-        if not self.validation_country:
-            raise ValueError("Укаазана недопустимая страна.")
-        self.country = country
+    adr = Address(post_index=1, country="Россия", city=1, district=1, street=1, home_number=1, flat_number=1)
+    user = User(name="user", last_name="test", nicname="user_test", age=15, email="young_troll@gmail.com", phone_number="89991234567", address=adr, login="login", password="password")
 
-        self.city = city
-        self.district = district
-        self.street = street
-        self.home_number = home_number
-        self.flat_number = flat_number
+    print(user.phone_number)
 
-    def validation_country(self, country):
-        """
-        Функция проверки существования страны (список стран дан просто для примера, в идеале,
-        список стран выделяется в отдельный файл и импортируется
-        :param country: страна пользователя
-        :return: True или False в зависимотси от успешности валидации
-        """
-        countries = ["Россия", "Соединенные Штаты Америки", "Германия", "Финляндия", "Казахстан", "Китай", "Алжир", "Япония"]
-        if country not in countries:
-            return False
-
-        return True
